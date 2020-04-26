a95aa9269fdf1135f2059e5d6f68663dcce62d3e
goetzb <goetz.buerkle@gmail.com>
2020-04-26 17:36:18 +0100

    Fixed JavaScript issue in admin nav sidebar.

--- django/contrib/admin/static/admin/js/nav_sidebar.js
+++ django/contrib/admin/static/admin/js/nav_sidebar.js
@@ -8,14 +8,17 @@
     }
     main.classList.toggle('shifted', navSidebarIsOpen === 'true');
 
-    toggleNavSidebar.addEventListener('click', function() {
-        'use strict';
-        if (navSidebarIsOpen == 'true') {
-            navSidebarIsOpen = 'false';
-        } else {
-            navSidebarIsOpen = 'true';
-        }
-        localStorage.setItem('django.admin.navSidebarIsOpen', navSidebarIsOpen);
-        main.classList.toggle('shifted');
-    });
+    if (toggleNavSidebar !== null) {
+        toggleNavSidebar.addEventListener('click', function() {
+            'use strict';
+            if (navSidebarIsOpen == 'true') {
+                navSidebarIsOpen = 'false';
+            } else {
+                navSidebarIsOpen = 'true';
+            }
+            localStorage.setItem('django.admin.navSidebarIsOpen', navSidebarIsOpen);
+            main.classList.toggle('shifted');
+        });
+    }
+    
 }
